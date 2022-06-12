4519526024399dbcb8053fa998cf4cae64639a72
medmoe <med.seffah@gmail.com>
2022-06-11 23:06:07 -0400

    create models

--- zemzem/models.py
+++ zemzem/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.TextField()
+    created = models.DateTimeField()
+
+
+class Provider(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.TextField()
+    create = models.DateTimeField()
+
+
+class Administrator(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.TextField()
+    create = models.DateTimeField()
