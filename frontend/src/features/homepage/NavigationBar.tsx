d98d43f1b18b9f31896bc25a876abe6144e20fbb
medmoe <med.seffah@gmail.com>
2022-06-21 12:41:54 -0400

    added enpoint for customer registration

---
+++ frontend/src/features/homepage/NavigationBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function NavigationBar () {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a >Home</a></li>
+                    <li><a >login</a></li>
+                    <li><a >signup</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
