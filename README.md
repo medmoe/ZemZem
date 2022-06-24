ce7853269f4e7072c7746e61b327abb1091305f3
medmoe <med.seffah@gmail.com>
2022-06-23 21:44:20 -0400

    finished authentication system

---
+++ README.md
@@ -0,0 +1,5 @@
+<img src="./frontend/src/assets/ZemZem.png">
+
+# ZemZem
+ZemZem is an application that connects customers to water suppliers 
+particularly in the regions that suffer from a lack of water resources.
--- frontend/src/features/homepage/NavigationBar.tsx
+++ frontend/src/features/homepage/NavigationBar.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import logo from './../../assets/ZemZem.png';
+import {useAppSelector} from "../../app/hooks";
+import { selectToken, selectUsername } from "../customer/customerSlice";
 import styles from './NavigationBar.module.css';
 export function NavigationBar () {
-    return (
-        <div className={styles.navbar}>
-            <img className={styles.logo} src={logo} alt="somename"/>
-            <nav>
-                <ul className={styles.navbar_links}>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/signup">signup</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">login</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+    const token = useAppSelector(selectToken);
+    const username = useAppSelector(selectUsername);
+    if(!token) {
+        return (
+            <div className={styles.navbar}>
+                <img className={styles.logo} src={logo} alt="logo"/>
+                <nav>
+                    <ul className={styles.navbar_links}>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">signup</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">login</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }else{
+        return (
+            <div className={styles.navbar}>
+                <img className={styles.logo} src={logo} alt="logo"/>
+                <nav>
+                    <ul className={styles.navbar_links}>
+                        <li><Link to="/">Home</Link></li>
+                        <li>Welcome, {username} !</li>
+                        <li>Logout</li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
 }
